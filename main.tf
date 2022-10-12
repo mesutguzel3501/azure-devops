fe3c1d15d7791dcacad2ff6586d61e5cea9d92bf
mesutguzel3501 <doganguzel@pythian.com>
2022-10-12 10:26:07 -0400

    added main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm"{
+   version = "3.0.0"
+    features  {}
+}
+# Create a resource group
+resource "azurerm_resource_group" "tf_test"{
+    name = "tfmainrg"
+    location = "Canada Central"
+    
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name     = "weahterapi"
+  location = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+
+   ip_address_type     = "Public"
+  dns_name_label      = "mesutguzelwa"
+  os_type             = "Linux"
+
+  container {
+    name   = "weatherapi"
+    image  = "mesutguzel/weatherapi"
+    cpu    = "0.5"
+    memory = "1"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+}
+
